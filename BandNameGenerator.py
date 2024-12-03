40d8281ecb450bde4b21d3a3ad138b486fcfc730
Sahana <sahanaa2602@gmail.com>
2024-12-03 11:39:42 +0530

    string concatenation

---
+++ BandNameGenerator.py
@@ -0,0 +1,4 @@
+city = input("enter the city name you grew up in")
+pet = input("enter your pet name")
+bandName = city + pet                                   #string concatenation
+print("your band name is ",bandName)
