3a892ed96f2ab44b39d82542cead89f4393d81fd
Sahana <sahanaa2602@gmail.com>
2024-12-12 12:50:49 +0530

    questions and answers

---
+++ QuizzGame/data.py
@@ -0,0 +1,102 @@
+question_data = [
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "text": "The HTML5 standard was published in 2014.",
+        "ans": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "text": "The first computer bug was formed by faulty wires.",
+        "ans": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "text": "FLAC stands for 'Free Lossless Audio Condenser'.",
+        "ans": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "text": "All program codes have to be compiled into an executable file in order to be run. This file can then be executed on any machine.",
+        "ans": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "easy",
+        "text": "Linus Torvalds created Linux and Git.",
+        "ans": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "easy",
+        "text": "The programming language 'Python' is based off a modified version of 'JavaScript'",
+        "ans": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "text": "AMD created the first consumer 64-bit processor.",
+        "ans": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "easy",
+        "text": "'HTML' stands for Hypertext Markup Language.",
+        "ans": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "easy",
+        "text": "In most programming languages, the operator ++ is equivalent to the statement '+= 1'.",
+        "ans": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "hard",
+        "text": "The IBM PC used an Intel 8008 microprocessor clocked at 4.77 MHz and 8 kilobytes of memory.",
+        "ans": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    }
+]
